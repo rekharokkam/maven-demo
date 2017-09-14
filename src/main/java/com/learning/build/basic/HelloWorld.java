027e3ade6ce373af885a6c49993cb496f2c5c192
Rekha Rokkam <rekha.rokkam@yahoo.com>
2017-09-14 14:32:44 -0700

    More lessons

---
+++ src/main/java/com/learning/build/basic/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.learning.build.basic;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World1");
+	}
+}
---
+++ src/nonstandard/java/Application2.java
@@ -0,0 +1,8 @@
+
+public class Application2
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
