610871c578eb24445e6b7b2dface884ab5edbb49
Rekha Rokkam <rekha.rokkam@yahoo.com>
2017-09-19 10:54:51 -0700

    next chapter related changes

---
+++ src/main/java/com/learning/build/basic/App.java
@@ -0,0 +1,25 @@
+package com.learning.build.basic;
+
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App
+{
+	private static boolean isNumeric (String nextLine){
+		return StringUtils.isNumeric(nextLine);
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("provie a valid Numeric value");
+		
+		if (isNumeric(scanner.nextLine())){
+			System.out.println("Provided string is numeric");
+		}
+		else{
+			System.out.println("your input is not valid");
+		}
+	}
+}
